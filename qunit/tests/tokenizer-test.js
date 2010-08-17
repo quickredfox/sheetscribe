c89d9b6b1891572f25b56c495708d1d3834f58e4
Quickredfox <code@quickredfox.at>
2010-08-17 15:31:28 -0400

    Working, tested and re-factored

--- qunit/tests/tokenizer-test.js
+++ qunit/tests/tokenizer-test.js
@@ -11,8 +11,8 @@ test('isNative',function() {
 });
 
 module("Extendend natives");
-test('Array.forEach',function() {
-    if(!isNative(Array.forEach)){
+test('Array.prototype.forEach',function() {
+    if(!isNative(Array.prototype.forEach)){
         var arr = [1,2,3],res=[];        
         arr.forEach(function(v) {
             res.push(v);
@@ -23,32 +23,44 @@ test('Array.forEach',function() {
     }
 });
 
-test('String.bisect',function() {
-    var stringToTest = "This is the first part {@#%^$%^&} This is the second part";
-    var t1 = stringToTest.bisect('{@#%^$%^&}');
-    same( t1[0], 'This is the first part ' , "should extract first part properly using a string delimiter");
-    same( t1[1], ' This is the second part', "should extract second part properly using a string delimiter");
-    ok(true,'Does not suport regex because it compiles one')
-});
-
 test('String.prototype.numOccurences',function() {
     var stringToTest = "AMC!@#$!@#A!@MDKBFDX$@%^AQK!@#$N@#K$JA!@#L$K!@L#$JKA!@#$KJ!@#$K!J@#$A";
     ok(stringToTest.numOccurences('A') == 6,'should count occurences of argument in string with one character');
     ok(stringToTest.numOccurences('MDKBFDX') == 1,'should count occurences of argument in string with multiple character');   
 });
 
-module("Data Objects")
-test('DeclarationObject.toString()',function() {
-    var sel = 'test';
-    var dec = new Declaration(sel);
-    same(dec.toString(),sel,'should be the selector')
+test('String.prototype.trim',function() {
+    if(!isNative(String.prototype.trim)){
+        var str = " test     ";
+        same(str.trim(),'test','Should trim whitespace from both ends')
+    }else{
+        ok(true,'trim is native, perfect!')
+    }
 });
-test('PropertyObject.toString()',function() {
-    var name = 'test';
-    var prop = new Property(name);    
-    same(prop.toString(),name,'should be the name')    
+
+module("Data Objects");
+var prop,dec,sheet;
+test('PropertyObject',function() {
+    var name = 'color';
+    prop = new Property(name,['red','blue']);    
+    same(prop.toString(),name,'toString() should be the name')   
+    same(prop.toCSS(),'color:red,blue;','toCSS() should generate proper rules')  
 });
-module("tokenizer")
+test('DeclarationObject',function() {
+    var sel = '#testselector';
+        dec = new Declaration(sel,[prop]);
+    same(dec.toString(),sel,'toString() should be the selector')
+    same(dec.toCSS(),'#testselector{color:red,blue;}','toCSS() should generate proper rules')      
+});
+test('SheetObject',function() {
+    var media = 'all';
+        sheet = new Sheet(media,[dec]);
+    same(sheet.toString(),media,'toString() should be the media')
+    same(sheet.toCSS(),'@media all{#testselector{color:red,blue;}}','toCSS() should generate proper rules')      
+});
+
+
+module("Tokenizer")
 test('integral()',function() {
     var testStringA = '{{{{{}}}}}';
     var testStringB = '{{{{{}}}}}}';    
@@ -76,13 +88,8 @@ test('clean()',function() {
     var exp = 'body{background:#00f;color:red;}.a,.b,#c,dd,p.e:first{padding:3px;border:1px solid red;}'
     equals(got,exp,"should clean out comments and such (suck-ass test really)");
 })
-test('snip()',function() {
-    equals(snip("  \
-    value\
-          "),'value','Should snip left and right whitespace off string')
-})
 test('munge()',function() {
-    same(munge("\
+    var got = munge("\
     @media dev, dev2 {\
      #shouldbedev { trueness: true; }\
      #shouldbedev2 { trueness: true;}\
@@ -99,22 +106,90 @@ test('munge()',function() {
     h1{\
         color:red;\
     }\
-    "),{
+    ")
+    var expected = {
         dev: [
-           {selector:"#shouldbedev",block:'trueness:true;'},
-          {selector:"#shouldbedev2",block:'trueness:true;'}
+           {"#shouldbedev":'trueness:true;',"#shouldbedev2":'trueness:true;'}
         ],
         dev2: [
-         {selector:"#shouldbedev",block:'trueness:true;'},
-         {selector:"#shouldbedev2",block:'trueness:true;'}
+         {"#shouldbedev":'trueness:true;',"#shouldbedev2":'trueness:true;'}
         ],
         screen: [
-         {selector:"#shouldbedev",block:'trueness:true;'},
+         {"#shouldbescreen":'trueness:true;'},
         ],
         all: [
-          {selector:'body',block:'mediaall:true;'},
-          {selector:'body p',block:'foo:bar;'},          
-          {selector:'h1',block:'color:red;'}                    
+          {'body':'mediaall:true;','body p':'foo:bar;','h1':'color:red;'}                    
         ]
-    },"should munge-out in proper media types")
+    }
+    ok(got.all,'Should have an "all" @media block')
+    ok(got.dev,'Should have a "dev" @media block')    
+    ok(got.dev2,'Should have a "dev" @media block')        
+    ok(got.screen,'Should have a "screen" @media block')            
+    same(got,expected,"Should munge as expected")
+})
+
+
+test('tokenize()/stringify()',function() {
+    var tokens = tokenize("\
+    @media dev, dev2 {\
+     #shouldbedev { trueness: true; }\
+     #shouldbedev2 { trueness: true;}\
+    }\
+    body{\
+        mediaall: true;\
+    }\
+    body p {\
+        foo:bar;\
+    }\
+    @media screen {\
+        #shouldbescreen { trueness: true;} \
+    }\
+    h1{\
+        color:red;\
+    }\
+    ");
+    var string = stringify(tokens);
+    var expected = "@media {#shouldbedev{trueness:true;}#shouldbedev2{trueness:true;}}@media {#shouldbedev{trueness:true;}#shouldbedev2{trueness:true;}}@media {#shouldbescreen{trueness:true;}}@media {body{mediaall:true;}body p{foo:bar;}h1{color:red;}}"
+    equals(string,expected,"Should tokenize and the stringify")
+})
+
+test('parse.assign()',function() {
+    var css = "\
+    @media dev, dev2 {\
+     #shouldbedev { trueness: true; }\
+     #shouldbedev2 { trueness: true;}\
+    }\
+    body{\
+        mediaall: true;\
+    }\
+    body p {\
+        foo:bar;\
+    }\
+    @media screen {\
+        #shouldbescreen { trueness: true;} \
+    }\
+    h1{\
+        color:red;\
+    }\
+    ";
+    var tokens = tokenize(css);
+    var proc = {foo:function() {}};
+    var form1 = parse.assign([css,'dev',proc]);
+    var form2 = parse.assign([css,proc]);    
+    var form3 = parse.assign([css,'dev']);    
+    var form4 = parse.assign([css]);        
+    
+    same( form1.tokens ,tokens,'Should put tokens in 1st place with arglength of 3')
+    same( form1.media ,'dev','Should put "media" in 2nd place with arglength of 3')    
+    same( form1.processors ,proc,'Should put "processors" in 3rd place with arglength of 3')     
+    
+    same( form2.media ,'all','Should assign media "all" when processors are 2nd argument')    
+    same( form2.processors ,proc,'Should shift arg pos of "processors" when assigning media "all" when processors are 2nd argument')        
+    
+    same( form3.processors ,{},'Should assign processors to "{}" when media is 2nd argument')    
+    same( form3.media ,'dev','Should still have everything in order...')        
+    
+    same( form4.tokens, tokens, "Should always parse tokens...")
+    same( form4.media, 'all', "Should always have a media...")
+    same( form4.processors, {}, "Should always have a processors object...")    
 })
